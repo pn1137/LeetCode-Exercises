b181c53a03efbc2968763a90bcafb66cf17006eb
pn1137 <pnaugle@mail.usf.edu>
2021-03-10 17:10:13 -0500

    Changed tabs to spaces

--- plus_one.cpp
+++ plus_one.cpp
@@ -1,39 +1,39 @@
 /*
-	Plus One
+  Plus One
 
-	Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+  Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
 
-	The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+  The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
 
-	You may assume the integer does not contain any leading zero, except the number 0 itself.
+  You may assume the integer does not contain any leading zero, except the number 0 itself.
 
-	 
+   
 
-	Example 1:
+  Example 1:
 
-	Input: digits = [1,2,3]
-	Output: [1,2,4]
-	Explanation: The array represents the integer 123.
-	Example 2:
+  Input: digits = [1,2,3]
+  Output: [1,2,4]
+  Explanation: The array represents the integer 123.
+  Example 2:
 
-	Input: digits = [4,3,2,1]
-	Output: [4,3,2,2]
-	Explanation: The array represents the integer 4321.
-	Example 3:
+  Input: digits = [4,3,2,1]
+  Output: [4,3,2,2]
+  Explanation: The array represents the integer 4321.
+  Example 3:
 
-	Input: digits = [0]
-	Output: [1]
-	 
+  Input: digits = [0]
+  Output: [1]
+   
 
-	Constraints:
+  Constraints:
 
-	1 <= digits.length <= 100
-	0 <= digits[i] <= 9
+  1 <= digits.length <= 100
+  0 <= digits[i] <= 9
 */
 
 
-///Runtime: 0 ms				// 100%
-///Memory Usage: 8.7 MB	// 83.73%
+///Runtime: 0 ms        // 100%
+///Memory Usage: 8.7 MB // 83.73%
 
 class Solution {
 public:
@@ -43,8 +43,8 @@ public:
             if (digits[i] == 9)
             {
                 digits[i] = 0;
-								
-                if (i == 0)	// Carry one
+                
+                if (i == 0) // Carry one
                 {
                     digits.insert(digits.begin(), 1);
                     break;
--- set_mismatch.cpp
+++ set_mismatch.cpp
@@ -1,32 +1,32 @@
 /*
-	645. Set Mismatch
-	
-	You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+  645. Set Mismatch
+  
+  You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
 
-	You are given an integer array nums representing the data status of this set after the error.
+  You are given an integer array nums representing the data status of this set after the error.
 
-	Find the number that occurs twice and the number that is missing and return them in the form of an array.
+  Find the number that occurs twice and the number that is missing and return them in the form of an array.
 
-	 
+   
 
-	Example 1:
+  Example 1:
 
-	Input: nums = [1,2,2,4]
-	Output: [2,3]
-	Example 2:
+  Input: nums = [1,2,2,4]
+  Output: [2,3]
+  Example 2:
 
-	Input: nums = [1,1]
-	Output: [1,2]
-	 
+  Input: nums = [1,1]
+  Output: [1,2]
+   
 
-	Constraints:
+  Constraints:
 
-	2 <= nums.length <= 104
-	1 <= nums[i] <= 104
+  2 <= nums.length <= 104
+  1 <= nums[i] <= 104
 */
 
-///Runtime: 32 ms					// 79.31%
-///Memory Usage: 21.4 MB	// 75.28%
+///Runtime: 32 ms         // 79.31%
+///Memory Usage: 21.4 MB  // 75.28%
 
 class Solution {
 public:
